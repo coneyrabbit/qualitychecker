3ecf017078f08b6d3558d55ca9e5d56d33c1d639
coneyrabbit <coneyrabbit@users.noreply.github.com>
2015-04-17 05:41:10 -0500

    Create addon.py

---
+++ addon.py
@@ -0,0 +1,11 @@
+﻿import xbmcaddon
+import xbmcgui
+ 
+addon       = xbmcaddon.Addon()
+addonname   = addon.getAddonInfo('name')
+ 
+line1 = "Program File: plugin.program.qualitychecker"
+line2 = "Code: Python"
+line3 = "Code Version: 0.0.1"
+ 
+xbmcgui.Dialog().ok(addonname, line1, line2, line3)
